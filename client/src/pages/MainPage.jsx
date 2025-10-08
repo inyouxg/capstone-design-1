e6436fc22bc8c3d4a6050a06aca3b1054a774f2d
inyouxg <dolley03@naver.com>
2025-10-08 16:56:05 +0900

    style: 메인 페이지 전체 스타일 구현
    하단 ButtonBar 제외

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,27 +1,33 @@
 import profileData from '../mock/profileData.json'
 import nutrientData from '../mock/nutrientData.json'
+import dietData from '../mock/dietData.json'
 import styles from './MainPage.module.css';
+import bubble from '../assets/bubble-icon.svg'
 import { useEffect, useState } from 'react';
 import { NutrientChartCard } from '../components/NutrientChartCard';
+import { DietReportCard } from '../components/DietReportCard';
+import { ButtonBar } from '../components/ButtonBar';
 
 export const MainPage = () => {
   const [profile, setProfile] = useState(null);
   const [nutrient, setNutrient] = useState(null);
-  const [dietReport, setDietReport] = useState(null);
+  const [diet, setDiet] = useState(null);
 
   useEffect(() => {
     setNutrient(nutrientData);
     setProfile(profileData);
+    setDiet(dietData);
   },[]);
 
   if (!nutrient) return null;
 
   return (
     <div className={styles['main-container']}>
+      <img className={styles['main-container__bubble']} src={bubble} alt='bubble-icon' />
       <div className={styles['main-container__header']}>
-        <h3>{profileData.name}</h3>
-        <div>
-          <p>권장 칼로리 | {profileData['recommanded-calories']}</p>
+        <h3>{profileData.name}님</h3>
+        <div className={styles['main-container__header-box']}>
+          <p>권장 칼로리 | {profileData['recommanded-calories']}kcal</p>
         </div>
       </div>
       <div className={styles['chart-container']}>
@@ -29,8 +35,15 @@ export const MainPage = () => {
           <NutrientChartCard nutrient={nutrient}/>
         </div>
       </div>
-      <div className={styles['report-container']}></div>
-      <div className={styles['header-container']}></div>
+      <div className={styles['diet-container']}>
+        <div className={styles['diet-container__section']}>
+          <h3>Diet Report</h3>
+          <DietReportCard diet={diet}/>
+        </div>
+      </div>
+      <div className={styles['button-container']}>
+        <ButtonBar />
+      </div>
     </div>
   )
 }
