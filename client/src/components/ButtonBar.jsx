795a5e65c42d26fcbaecbc224962b7cab975dfeb
inyouxg <dolley03@naver.com>
2025-11-01 15:31:50 +0900

    feat: imageUploading 기능 구현

--- client/src/components/ButtonBar.jsx
+++ client/src/components/ButtonBar.jsx
@@ -1,22 +1,48 @@
+import { useState } from 'react'
+import ImageUploading from 'react-images-uploading'
 import today from '../assets/today-icon.svg'
 import report from '../assets/report-icon.svg'
 import gallery from '../assets/gallery-icon.svg'
 import styles from './ButtonBar.module.css'
 
 export const ButtonBar = () => {
+  const [image, setImage] = useState(null);
+
+  const onChange = (imageList) => {
+    if (imageList.length > 0) {
+      const selectedImage = imageList[0];
+      console.log("selected-image: ", selectedImage);
+      setImage(selectedImage);
+    }
+  };
+
   return (
-    <nav className={styles['nav-container']}>
-      <button className={styles['nav-container__button']}>
-        <img src={gallery}/>
-      </button>
-      <a className={styles['nav-container__item']}>
-        <img src={today}></img>
-        <span>today</span>
-      </a>
-      <a className={styles['nav-container__item']}>
-        <img src={report}></img>
-        <span>report</span>
-      </a>
-    </nav>
+    <ImageUploading
+      value={image ? [image] : []}
+      onChange={onChange}
+      dataURLKey="data_url"
+      acceptType={["jpg", "png", "jpeg"]}
+    >
+      {({ onImageUpload }) => (
+        <nav className={styles["nav-container"]}>
+          <button
+            onClick={onImageUpload}
+            className={styles["nav-container__button"]}
+          >
+            <img src={gallery} alt="gallery" />
+          </button>
+
+          <a className={styles["nav-container__item"]}>
+            <img src={today} alt="today" />
+            <span>today</span>
+          </a>
+
+          <a className={styles["nav-container__item"]}>
+            <img src={report} alt="report" />
+            <span>report</span>
+          </a>
+        </nav>
+      )}
+    </ImageUploading>
   )
 }
