b71885bd6fb4ff4c1baf71371c1e179f0911019d
inyouxg <dolley03@naver.com>
2025-11-03 18:18:51 +0900

    feat: modalContext 생성

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
+import { ModalProvider } from './contexts/ModalContext'
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <ModalProvider>
+      <RouterProvider router={router} />
+    </ModalProvider>
   )
 }
 
---
+++ client/src/contexts/ModalContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useState } from "react";
+
+const ModalContext = createContext();
+
+export const useModal = () => useContext(ModalContext);
+
+export const ModalProvider = ({ children }) => {
+  const [modalContent, setModalContent] = useState(null);
+
+  const openModal = (content) => {
+    setModalContent(content);
+  };
+  const closeModal = () => {
+    setModalContent(null);
+  };
+
+  return (
+    <ModalContext.Provider value={{ openModal, closeModal }}>
+      {children}
+      {modalContent && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "rgba(0,0,0,0.4)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 1000,
+            
+          }}
+        >
+          {modalContent}
+        </div>
+      )}
+    </ModalContext.Provider>
+  );
+};
